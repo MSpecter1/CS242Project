24b8d46d75261159cbf06054e05f251266b0a757
huytran2511 <huyqtran96@gmail.com>
2024-03-09 19:33:33 -0800

    incorporate pylucene to webapp

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -3,10 +3,14 @@ import json
 import pandas as pd
 import numpy as np
 from bert_index import *
+from pylucene_search import *
 
 list_data_obj = json.load(open("../CS242 Final Combined IMDB Dataset (nulls replaced).json"))
 search_index = search_bert_index()
 search_index.read_index("multi_full_index_no_mask.index")
+
+pylucene_search_index = search_pylucene_index()
+pylucene_index = "imdb_lucene_index/"
 
 class test_obj:
     def __init__(self, x, y):
@@ -15,23 +19,55 @@ class test_obj:
 
 def method_call(radio_button, num_results, inp):
     if radio_button == "PyLucene":
+        # place_holder_obj = [
+        #     test_obj(1, 1),
+        #     test_obj(2, 2),
+        #     test_obj(3, 3)
+        # ]
+        # movie_pylucene_results = [
+        #     {
+        #         "score": 0.95,
+        #         "title": "The Shawshank Redemption",
+        #         "synopsis": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+        #         "region": "USA",
+        #         "start_year": "1994",
+        #         "genres": ["Drama"],
+        #         "directors_names": ["Frank Darabont"],
+        #         "writer_names": ["Stephen King", "Frank Darabont"]
+        #     },
+        #     {
+        #         "score": 0.92,
+        #         "title": "The Godfather",
+        #         "synopsis": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        #         "region": "USA",
+        #         "start_year": "1972",
+        #         "genres": ["Crime", "Drama"],
+        #         "directors_names": ["Francis Ford Coppola"],
+        #         "writer_names": ["Mario Puzo", "Francis Ford Coppola"]
+        #     }
+        # ]
+
+        movie_pylucene_results = pylucene_search_index.retrieve(inp, num_results=num_results)
+
+
+        # return pd.DataFrame([t.__dict__ for t in place_holder_obj])
+        return pd.DataFrame(movie_pylucene_results)
+
+    elif radio_button == "Bert":
+        # movie_indexes = search_index.search(inp, num_results=num_results)
+        # movie_bert_results = []
+        # for index in movie_indexes[0]:
+        #     movie_bert_results.append(list_data_obj[index])
+        # for i in range(len(movie_bert_results)):
+        #     t_const_id = movie_bert_results[i]["tconst"]
+        #     movie_bert_results[i]["URL"] = f"https://www.imdb.com/title/{t_const_id}"
         place_holder_obj = [
             test_obj(1, 1),
             test_obj(2, 2),
             test_obj(3, 3)
         ]
         return pd.DataFrame([t.__dict__ for t in place_holder_obj])
-
-    elif radio_button == "Bert":
-        movie_indexes = search_index.search(inp, num_results=num_results)
-        movie_bert_results = []
-        for index in movie_indexes[0]:
-            movie_bert_results.append(list_data_obj[index])
-        for i in range(len(movie_bert_results)):
-            t_const_id = movie_bert_results[i]["tconst"]
-            movie_bert_results[i]["URL"] = f"https://www.imdb.com/title/{t_const_id}"
-        #return pd.DataFrame([t.__dict__ for t in place_holder_obj])
-        return pd.DataFrame(movie_bert_results)
+        # return pd.DataFrame(movie_bert_results)
 
     return pd.DataFrame([{
         "Error": "Make Sure to Select A search Method"

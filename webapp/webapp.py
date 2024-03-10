3fc5129759372ac618f5722243ff5d88da505295
huytran2511 <huyqtran96@gmail.com>
2024-03-09 20:03:18 -0800

    added instruction for pylucene in webapp

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -54,34 +54,62 @@ def method_call(radio_button, num_results, inp):
         return pd.DataFrame(movie_pylucene_results)
 
     elif radio_button == "Bert":
-        # movie_indexes = search_index.search(inp, num_results=num_results)
-        # movie_bert_results = []
-        # for index in movie_indexes[0]:
-        #     movie_bert_results.append(list_data_obj[index])
-        # for i in range(len(movie_bert_results)):
-        #     t_const_id = movie_bert_results[i]["tconst"]
-        #     movie_bert_results[i]["URL"] = f"https://www.imdb.com/title/{t_const_id}"
-        place_holder_obj = [
-            test_obj(1, 1),
-            test_obj(2, 2),
-            test_obj(3, 3)
-        ]
-        return pd.DataFrame([t.__dict__ for t in place_holder_obj])
-        # return pd.DataFrame(movie_bert_results)
+        movie_indexes = search_index.search(inp, num_results=num_results)
+        movie_bert_results = []
+        for index in movie_indexes[0]:
+            movie_bert_results.append(list_data_obj[index])
+        for i in range(len(movie_bert_results)):
+            t_const_id = movie_bert_results[i]["tconst"]
+            movie_bert_results[i]["URL"] = f"https://www.imdb.com/title/{t_const_id}"
+        # place_holder_obj = [
+        #     test_obj(1, 1),
+        #     test_obj(2, 2),
+        #     test_obj(3, 3)
+        # ]
+        # return pd.DataFrame([t.__dict__ for t in place_holder_obj])
+        return pd.DataFrame(movie_bert_results)
 
     return pd.DataFrame([{
         "Error": "Make Sure to Select A search Method"
     }])
 
-demo = gr.Interface(
-    fn=method_call,
-    inputs=[
-        gr.Radio(["PyLucene", "Bert"], label="Search Method", info="Choose how to look up"),
-        gr.Number(value=0, label="Number Results", info="How many Results"),
-        gr.Textbox(placeholder="Horror Movies", label="Search Engine", info="What you are looking up")
-    ],
-    outputs="dataframe"
-)
 
+def change_instructions(choice):
+    if choice == "PyLucene":
+        return gr.Markdown("""
+                            #### PyLucene Search Instructions:
+                            > For a basic text search, enter a search query with the format `field`:`search_val`
+
+                            > For a range search on `start_year`, enter a search query with the format `start_year`:`YYYY TO YYYY`
+
+                            > For a wildcard search, enter a search query with the format `field`:`value`
+
+                            > For field/term boosting, add ^X.X after the search value
+                            """)
+    else:
+        return gr.Markdown(
+                            """
+                            #### Bert Search Instructions:
+                            > Enter your query in the textbox below
+                            """)
+
+with gr.Blocks() as demo:
+    radio_button = gr.Radio(["PyLucene", "Bert"], value="PyLucene", label="Search Method")
+    num_results = gr.Number(value=0, label="Number of Results")
+    instructions = gr.Markdown("""
+                                #### PyLucene Search Instructions:
+                                > For a basic text search, enter a search query with the format `field`:`search_val`
+
+                                > For a range search on `start_year`, enter a search query with the format `start_year`:`YYYY TO YYYY`
+
+                                > For a wildcard search, enter a search query with the format `field`:`value`
+
+                                > For field/term boosting, add ^X.X after the search value
+                                """)
+    inp = gr.Textbox(placeholder="Search...", label="Search Engine")
+    submit = gr.Button(value="Submit", variant="primary")
+    submit.click(fn=method_call, inputs=[radio_button, num_results, inp], outputs=gr.Dataframe())
+    radio_button.change(fn=change_instructions, inputs=radio_button, outputs=instructions)
+    
 if __name__ == "__main__":
     demo.launch(server_port=8080)
